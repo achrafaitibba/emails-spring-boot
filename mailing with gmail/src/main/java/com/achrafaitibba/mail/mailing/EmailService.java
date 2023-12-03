4100bcedcded5adcda688c76efcaddb447f06ac8
Achraf Ait Ibba <97747197+achrafaitibba@users.noreply.github.com>
2023-12-03 06:00:21 +0100

    Add files via upload

---
+++ mailing with gmail/src/main/java/com/achrafaitibba/mail/mailing/EmailService.java
@@ -0,0 +1,15 @@
+package com.achrafaitibba.mail.mailing;
+
+// Importing required classes
+
+// Interface
+public interface EmailService {
+
+    // Method
+    // To send a simple email
+    String sendSimpleMail(EmailDetails details);
+
+    // Method
+    // To send an email with attachment
+    String sendMailWithAttachment(EmailDetails details);
+}
